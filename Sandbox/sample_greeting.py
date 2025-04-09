c4d7592c60183046b3fd47172231dfa2fa0c33da
Greg Tate <gregt8@outlook.com>
2025-04-09 03:40:08 -0500

    Add sample_greeting module with user greeting and age checking functionality

---
+++ Sandbox/sample_greeting.py
@@ -0,0 +1,19 @@
+def greet_user():
+    name = input("What's your name? ")
+    print(f"Hello, {name}!")
+
+def check_age():
+    try:
+        age = int(input("How old are you? "))
+        if age < 18:
+            print("You're a minor.")
+        elif age < 65:
+            print("You're an adult.")
+        else:
+            print("You're a senior citizen.")
+    except ValueError:
+        print("Please enter a valid number.")
+
+# Main program
+greet_user()
+check_age()

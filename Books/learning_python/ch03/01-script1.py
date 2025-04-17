232e3840e5e848b06a4ce94d32f49296aba8cf29
Greg Tate <gregt8@outlook.com>
2025-04-17 03:03:38 -0500

    Ch03: Add simple platform and math demo script

---
+++ Books/learning_python/ch03/01-script1.py
@@ -0,0 +1,10 @@
+# Program: Simple platform and math demo
+# Context: Learning Python, Chapter 3, Script 1 (Mark Lutz)
+# Author:  Greg Tate
+# Date:    2024-04-27
+# Synopsis: Prints the current platform, computes 2 to the power of 100, and repeats a string multiple times.
+import sys
+print(sys.platform)
+print(2 ** 100)
+x = 'Hack!'
+print(x * 8)
